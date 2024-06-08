6a4fda26fb76d8b7098bbe4e268f2d6fdf1473fa
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-06-08 17:37:22 -0500

    Create the files for pages that will be shown on the app. Also added them to App.js

--- fitcast/src/App.js
+++ fitcast/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <></>
+    <>
+      <Homepage />
+      <Wardrobe />
+    </>
   );
 }
 
---
+++ fitcast/src/pages/Wardrobe.jsx
@@ -0,0 +1,7 @@
+export default function Wardrobe() {
+  return (
+    <div>
+      <h1>Wardrobe</h1>
+    </div>
+  );
+}
