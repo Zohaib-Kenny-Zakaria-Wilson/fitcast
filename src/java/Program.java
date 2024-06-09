67276cd3b9f42002212ef9e97667fb3d4a53b3ca
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-06-09 13:15:36 -0500

    The response from the API is working however we don't have any logic for what to do with the information.

--- src/java/Program.java
+++ src/java/Program.java
@@ -6,7 +6,7 @@ import java.net.http.HttpResponse;
 import java.net.URI;
 import java.io.IOException;
 import credentials.Constants;
-import com.google.gson.Gson;
+// import com.google.gson.Gson;
 import java.util.Map;
 import java.util.Set;
 
@@ -25,12 +25,12 @@ public class Program {
         HttpClient client = HttpClient.newHttpClient();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body()); 
-        Map<String, Object> json = new Gson().fromJson(response.body(), Map.class);
-        Set<String> keys = json.keySet();
-        for (String key : keys) {
-            System.out.print(key + ": " + json.get(key) + "----------     ");
-            System.out.println(json.get(key));
-        }
+        // Map<String, Object> json = new Gson().fromJson(response.body(), Map.class);
+        // Set<String> keys = json.keySet();
+        // for (String key : keys) {
+        //     System.out.print(key + ": " + json.get(key) + "----------     ");
+        //     System.out.println(json.get(key));
+        // }
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
