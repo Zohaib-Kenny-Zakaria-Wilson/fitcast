cd1fbef94a2e04e5ae9bc1e644dd719709063058
Your Name <knnymrls@outlook.com>
2024-06-09 13:51:56 -0500

    refactor: Update WeatherKeyInfo component styling for responsiveness

--- src/components/WeatherKeyInfo.jsx
+++ src/components/WeatherKeyInfo.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function WeatherKeyInfo({ svg, text }) {
   return (
-    <div className="w-full px-2 py-3 flex justify-center gap-2 rounded-lg bg-background dark:bg-dark-background text-primary dark:text-dark-primary">
+    <div className="w-full px-2 py-3 2xl:px-4 2xl:py-4 flex justify-center gap-2 rounded-lg bg-background dark:bg-dark-background text-primary dark:text-dark-primary">
       <img
-        className="h-4"
+        className="h-4 2xl:h-5"
         src={`${process.env.PUBLIC_URL}/assets/${svg}.svg`}
         alt={svg}
       />
-      <p className="text-sm">{text}</p>
+      <p className="text-sm 2xl:text-base">{text}</p>
     </div>
   );
 }
