f5fac3f5679879d02e97fcf4aa716f48787471cb
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-06-09 15:20:12 -0500

    Requires a text component to create this component. Not providing an svg is fine as long as there is text, which will be used to display the functionality of the button.
    svg is initlaly set to null because it is not required. If it is provided, it will be used for the component.

--- src/components/WeatherKeyInfo.jsx
+++ src/components/WeatherKeyInfo.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function WeatherKeyInfo({ svg, text }) {
+export default function WeatherKeyInfo({ svg = null, text }) {
   return (
     <div className="w-full px-1 py-2 xl:px-2 xl:py-3 2xl:px-4 2xl:py-4 flex justify-center gap-2 rounded-lg bg-background dark:bg-dark-background text-primary dark:text-dark-primary">
-      <img
+      if (svg !== null) {
+        <img
         className="h-4 2xl:h-5"
         src={`${process.env.PUBLIC_URL}/assets/${svg}.svg`}
         alt={svg}
       />
+      }
       <p className="text-xs xl:text-sm 2xl:text-base">{text}</p>
     </div>
   );
 }
 
-export default WeatherKeyInfo;
+WeatherKeyInfo.propTypes = {
+  text: PropTypes.node.isRequired,
+};
