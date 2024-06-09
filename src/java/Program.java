cf116e3cfad11ec6299c6f3f39f24a6f5d280052
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-06-09 10:34:55 -0500

    Java backend should now work with Maven installed. The backend is now ready to interact with APIs.

--- src/java/Program.java
+++ src/java/Program.java
@@ -1,5 +1,5 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Backend now changed to java");
+        System.out.println("Hello, World!");
     }
 }
