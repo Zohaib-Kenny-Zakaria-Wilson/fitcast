7bae4efaf5631e7053c9ae70e61a5c8a0ea85366
Kenny Morales <kenny-morales@outlook.com>
2024-06-08 19:06:37 -0500

    refactor: Add onClick prop to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ children }) {
+function Button({ onClick, children }) {
   return (
     <button className="px-4 py-2 rounded-md bg-neutral-200 box-border text-neutral-800 border-b-2 border-neutral-400 hover:border-b-0 ">
         {children}
@@ -10,7 +10,8 @@ function Button({ children }) {
 }
 
 Button.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
