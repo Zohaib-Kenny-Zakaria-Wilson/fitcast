573a2b328074d822c3f8ec1808dab5132f6c115a
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-06-09 10:49:14 -0500

    Created a credentials file which stores the API Key. It is also mentioned in the git ignore so that people who look at this repository will not find the API key.

--- src/java/Program.java
+++ src/java/Program.java
@@ -1,5 +1,11 @@
+import java.net.http.HttpRequest;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String url  = "http://api.weatherapi.com/v1"
+        HttpRequest request = new HttpRequest.newBuilder()
+            .uri(new URI(url))
+            .GET()
+            .build();
     }
 }
