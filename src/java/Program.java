e5c8f09ca3152f15f78b6e2f374ab353b3a77680
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-06-09 12:01:32 -0500

    Built request for WeatherAPI.

--- src/java/Program.java
+++ src/java/Program.java
@@ -1,12 +1,27 @@
+package src.java;
+
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
+import java.io.IOException;
 
 public class Program {
     public static void main(String[] args) {
         // PLACEHOLDER FOR NOW. WILL BE USED TO MAKE API CALLS. NEEDS TO COME FROM THE
         // FRONTEND.
         final String LOCATION = "68508";
-        String url = "http://api.weatherapi.com/v1/current.json?key=" + Constants.API_KEY + "&q=" + LOCATION;
-        HttpRequest request = new HttpRequest.newBuilder()
+        String url = "http://api.weatherapi.com/v1/current.json?key=" + Constants.API_KEY.toString() + "&q=" + LOCATION;
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(url))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
