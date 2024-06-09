36f9a1a3ddbf36bcd31a3b35b430fffcaeba93c9
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-06-09 11:10:36 -0500

    Built the API key request URL. Now working on building the HTTP request to Weather API

--- src/java/Program.java
+++ src/java/Program.java
@@ -2,10 +2,11 @@ import java.net.http.HttpRequest;
 
 public class Program {
     public static void main(String[] args) {
-        String url  = "http://api.weatherapi.com/v1"
+        // PLACEHOLDER FOR NOW. WILL BE USED TO MAKE API CALLS. NEEDS TO COME FROM THE
+        // FRONTEND.
+        final String LOCATION = "68508";
+        String url = "http://api.weatherapi.com/v1/current.json?key=" + Constants.API_KEY + "&q=" + LOCATION;
         HttpRequest request = new HttpRequest.newBuilder()
-            .uri(new URI(url))
-            .GET()
-            .build();
+                .uri(new URI(url))
     }
 }
