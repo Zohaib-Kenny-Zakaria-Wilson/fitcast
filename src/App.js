2343b64ebc3d20161ee66219f6f0b800cf69b66c
Your Name <knnymrls@outlook.com>
2024-06-08 19:41:17 -0500

    feat: Add Sidebar component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Homepage from './pages/Homepage';
-import Wardrobe from './pages/Wardrobe';
+import Sidebar from './components/Sidebar';
 import Button from './components/Button';
 import './App.css';
 
 function App() {
   return (
     <>
+      <Sidebar />
       <h1 className="text-teal-500">
         Hello world!
       </h1>
