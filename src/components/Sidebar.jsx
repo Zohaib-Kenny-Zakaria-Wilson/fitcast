5915fabeec281edf5d8a6e4c7326503fa1d5d37b
Your Name <knnymrls@outlook.com>
2024-06-08 20:42:23 -0500

    Got the main section of the display going

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 function WeatherComponent() {
     return (
         <div className="bg-foreground w-fit rounded-lg min-h-full">
-            <div className='w-80 py-16 px-6'>
-                <div className="w-full flex flex-col items-center">
-                    <p className='text-center font-bold text-3xl text-primary'>Grand Island</p>
+            <div className='w-fit py-16 px-6 2xl:px-12 '>
+                <div className="w-full flex flex-col gap-6 items-center">
+                    <p className='text-center font-bold text-3xl  text-primary'>Grand Island</p>
+                    <img className='size-48  -my-5 ' src={`${process.env.PUBLIC_URL}/assets/clear-day.svg`} alt="" />
+                    <div className='flex flex-col gap-2'>
+                    <p className='text-center font-bold text-5xl text-primary'>75°F</p>
+                    <p className="text-center text-base  text-primary">Sunny</p>
+                    </div>
                 </div>
             </div>
           </div>
