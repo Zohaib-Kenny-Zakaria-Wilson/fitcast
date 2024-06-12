08f325f05c1e22b47010254ae5f389ee533ea75c
Kenny Morales <kenny-morales@outlook.com>
2024-06-12 18:14:02 -0500

    Updating the background color and text color to be correspondant do the clothing item color and make the styling more inline with portrait pictures

--- src/components/ItemComponents/ItemMini.jsx
+++ src/components/ItemComponents/ItemMini.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 
+const PLACEHOLDER_IMAGE_URL = "https://via.placeholder.com/1170x2532";
+
 export default function ItemMini({ clothingItem }) {
   return (
     <main
-      className={`flex flex-col w-48 h-48 rounded-md`}
+      className={`flex flex-col  rounded-md`}
       style={{ backgroundColor: clothingItem.dominantColor }}
     >
-      <div className="w-full h-5/6">
+      <div className="w-48 h-48">
         <img
           className="object-cover object-center w-full h-full rounded-t-md"
-          src={`${process.env.PUBLIC_URL}/assets/${clothingItem.imageURL}`}
+          src={clothingItem.imageURL || PLACEHOLDER_IMAGE_URL}
           alt=""
         />
       </div>
-      <div className="flex items-center justify-center w-full h-2/5">
-        <p className="text-center " style={{ color: clothingItem.contrastColor }}>
+      <div className="flex items-center justify-center w-full py-5 bg-opacity-50 rounded-b-md h-fit" style={{ backgroundColor: clothingItem.dominantColor }}>
+        <p className="text-center" style={{ color: clothingItem.contrastColor }}>
           {clothingItem.name}
         </p>
       </div>
