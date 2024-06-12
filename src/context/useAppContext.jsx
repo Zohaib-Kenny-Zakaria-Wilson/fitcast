d0164791bd14e9cb8bf004e763549dfb1df6d70f
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-06-11 20:49:53 -0500

    creates context file with boilerplate code

---
+++ src/context/useAppContext.jsx
@@ -0,0 +1,14 @@
+import { useContext, createContext, useState } from "react";
+export const AppContext = createContext(null);
+export default function useAppContext() {
+  return useContext(AppContext);
+}
+export function AppProvider(props) {
+  const [clothingItems, setClothingItems] = useState([]);
+
+  return (
+    <AppContext.Provider value={{ clothingItems, setClothingItems }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+}
