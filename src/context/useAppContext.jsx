7309734f4d924450d8ebe1f37b73540daa94481b
Kenny Morales <kenny-morales@outlook.com>
2024-06-12 09:54:52 -0500

    Implemented the class on appContext

--- src/context/useAppContext.jsx
+++ src/context/useAppContext.jsx
@@ -1,10 +1,14 @@
 import { useContext, createContext, useState } from "react";
+import { clothingItem } from "../components/models/clothingItem";
+
 export const AppContext = createContext(null);
 export default function useAppContext() {
   return useContext(AppContext);
 }
 export function AppProvider(props) {
-  const [clothingItems, setClothingItems] = useState([{ text: "fancy Shirt" }]);
+  const [clothingItems, setClothingItems] = useState([
+    new clothingItem("1", "1", "White T-Shirt", "Red", "Black", "Shirt", "tshirt.png", true, false, true, false, true, false, true, false)
+  ]);
 
   return (
     <AppContext.Provider value={{ clothingItems, setClothingItems }}>
