0165fe0b1d8cc8bfc6e6aa4d07cebb95c4253f4f
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-06-11 20:49:30 -0500

    adds wrapper in index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { AppProvider } from "./context/useAppContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AppProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AppProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
