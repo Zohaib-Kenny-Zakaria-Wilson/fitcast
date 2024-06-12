054b2b45ab7a846a5b07901a1d94f29c4279ee50
Kenny Morales <kenny-morales@outlook.com>
2024-06-12 13:32:19 -0500

    Added context to the clothingItem component and made a new editItem function to map thru the old one

--- src/components/ItemComponents/ClothingItem.jsx
+++ src/components/ItemComponents/ClothingItem.jsx
@@ -1,36 +1,65 @@
 import React, { useState } from "react";
 import ItemMini from "./ItemMini";
 import ItemExpanded from "./ItemExpanded";
+import useAppContext from "../../context/useAppContext";
+import { clothingItem } from "../models/clothingItem";
 
-export default function ClothingItem({ link, text, color, textColor }) {
+/**
+ * 
+ * @param {clothingItem} The clothing item to display in the wardrobe
+ */
+export default function ClothingItem({ clothingItem }) {
+  // State to toggle the modal
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  // Used to update the clothing items in the context when edited
+  const { setClothingItems } = useAppContext();
+
+
+  /**
+   * Opens the modal
+   *
+   * @returns {void}
+   */
   const openModal = () => {
     setIsModalOpen(true);
   };
 
+  /**
+   * Closes the modal
+   * 
+   * @returns {void}
+   */
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  const handleFormSubmit = (formData) => {
-    console.log("Form submitted with:", formData);
+  /**
+   * Edits the item in the wardrobe and updates the context
+   * @param {clothingItem} newClothingItem The new clothing item to replace the old one
+   * 
+   * @returns {void}
+   */
+  const editItem = (newClothingItem) => {
+    setClothingItems((prevItems) => 
+      prevItems.map((item) => 
+        item.id === clothingItem.id
+          ? { ...item, name: newClothingItem.text, category: newClothingItem.selectedOption }
+          : item
+      )
+    );
     closeModal();
   };
-
   return (
     <>
       <button type="button" onClick={openModal}>
-        <ItemMini link={link} text={text} color={color} textColor={textColor} />
+        <ItemMini link={clothingItem.image} text={clothingItem.name} color={clothingItem.dominantColor} textColor={clothingItem.contrastColor} />
       </button>
       {isModalOpen && (
         <dialog open className="modal">
           <ItemExpanded
-            link={link}
-            parentText={text}
-            color={color}
-            textColor={textColor}
-            onSubmit={handleFormSubmit}
+            clothingItem={clothingItem}
+            onSubmit={editItem}
           />
           <button type="button" onClick={closeModal} className="modal-backdrop backdrop-blur-sm">
             Close
