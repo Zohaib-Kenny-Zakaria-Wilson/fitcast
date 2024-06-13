50a539fd61f6be6a135d7deba75695ed85bfddf4
Kenny Morales <kenny-morales@outlook.com>
2024-06-12 22:17:47 -0500

    feat: Add AddItem component to Wardrobe page

--- src/components/Wardrobe.jsx
+++ src/components/Wardrobe.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import useAppContext from "../context/useAppContext";
 import ClothingItem from "./ItemComponents/ClothingItem";
+import AddItem from '../components/ItemComponents/AddItem';
+
 
 export default function Wardrobe() {
     const { clothingItems } = useAppContext();
 
     return (
     <>
-    <div className="flex flex-wrap justify-start p-12 rounded-lg gap-9 bg-foreground">
+    <div className="flex flex-wrap justify-start h-screen p-12 rounded-lg gap-9 bg-foreground">
+    <AddItem />
         {clothingItems &&
           clothingItems.map((clothingItem) => {
             return (
