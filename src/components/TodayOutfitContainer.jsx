74c626581bb5a518a349bb39753e6de4485c8a1f
Kenny Morales <kenny-morales@outlook.com>
2024-06-12 20:48:51 -0500

    Added a link to wardrobe

--- src/components/TodayOutfitContainer.jsx
+++ src/components/TodayOutfitContainer.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Outfit from "./OutfitComponents/Outfit";
 
 export default function TodayOutfitContainer() {
   return (
-    <>
     <main className="w-full h-full">
+       <Link to="/wardrobe">
+          <button className="px-4 py-2 mt-4 rounded text-background bg-primary-tw hover:bg-blue-700">
+            Wardrobe
+          </button>
+        </Link>
       <div className="flex flex-col p-12 rounded-lg gap-9 bg-foreground">
         <Outfit />
+       
       </div>
     </main>
-    </>
   );
 }
