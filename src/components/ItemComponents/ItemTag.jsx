b16c953d7dc56e61adfd1f079eabe8b6bdf51f7a
Kenny Morales <kenny-morales@outlook.com>
2024-06-12 22:18:08 -0500

    refactor: Update ItemTag component with cursor pointer and remove unused styles

--- src/components/ItemComponents/ItemTag.jsx
+++ src/components/ItemComponents/ItemTag.jsx
@@ -4,8 +4,8 @@ export default function ItemTag({ onClick, text, color, textColor, borderColor }
   return (
     <div
       onClick={onClick}
-      className="flex justify-center gap-2 px-1 py-1 border-2 rounded-lg w-fit xl:px-2 xl:py-2 2xl:px-4 2xl:py-3"
-      style={{ backgroundColor: color, color: textColor, borderColor: borderColor}}
+      className="flex justify-center gap-2 px-1 py-1 border-2 rounded-lg cursor-pointer w-fit xl:px-2 xl:py-2 2xl:px-4 2xl:py-3"
+      style={{ backgroundColor: color, color: textColor, borderColor: borderColor }}
     >
       <p className="text-xs xl:text-sm 2xl:text-base">{text}</p>
     </div>
