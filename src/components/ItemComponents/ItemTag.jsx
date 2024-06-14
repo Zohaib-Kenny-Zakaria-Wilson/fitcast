0281beb0839ebab0356aa54f4c575a1bdf7f7038
Kenny Morales <kenny-morales@outlook.com>
2024-06-13 23:57:14 -0500

    Removed unnecessary css

--- src/components/ItemComponents/ItemTag.jsx
+++ src/components/ItemComponents/ItemTag.jsx
@@ -7,7 +7,7 @@ export default function ItemTag({ onClick, text, color, textColor, borderColor }
       className="flex justify-center gap-2 px-1 py-1 border-2 rounded-lg cursor-pointer w-fit xl:px-2 xl:py-2 2xl:px-4 2xl:py-3"
       style={{ backgroundColor: color, color: textColor, borderColor: borderColor }}
     >
-      <p className="text-xs xl:text-sm 2xl:text-base">{text}</p>
+      <p className="text-xs 2xl:text-base">{text}</p>
     </div>
   );
 }
