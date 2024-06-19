d270590d0939e6ba31f346511481126cadb8b1fa
Your Name <knnymrls@outlook.com>
2024-06-18 22:52:29 -0500

    Set up basic HTML

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-export default function TodayOutfitContainer() {
+export default function navBar() {
   return (
-    <></>
+    <>
+    <main>
+        {/* Search Bar*/}
+        <div></div>
+        {/* Dark Mode Button */}
+        <button></button>
+        { /* Profile */}
+        <button></button>
+    </main>
+    </>
   );
 }
