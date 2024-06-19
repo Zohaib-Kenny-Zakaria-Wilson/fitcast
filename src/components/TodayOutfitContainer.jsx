594d107deb1f7b26b74ae396420733bbffeeb5c8
Kenny Morales <kenny-morales@outlook.com>
2024-06-19 00:48:35 -0500

    Fixed the Dark Mode on Today Outfit Container

--- src/components/TodayOutfitContainer.jsx
+++ src/components/TodayOutfitContainer.jsx
@@ -6,11 +6,11 @@ export default function TodayOutfitContainer() {
   return (
     <main className="w-full h-full">
        <Link to="/wardrobe">
-          <button className="px-4 py-2 mt-4 rounded text-background bg-primary-tw hover:bg-blue-700">
+          <button className="px-4 py-2 mt-4 rounded text-background dark:text-dark-background bg-primary-tw dark:bg-dark-primary-tw hover:bg-blue-700">
             Wardrobe
           </button>
         </Link>
-      <div className="flex flex-col p-12 rounded-lg gap-9 bg-foreground">
+      <div className="flex flex-col p-12 rounded-lg gap-9 dark:bg-dark-foreground bg-foreground">
         <Outfit />
        
       </div>
