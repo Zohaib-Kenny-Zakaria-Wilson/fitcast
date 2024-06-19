ed26534fbcd32fc84bbbee9437e0d6fbefa3f8cd
Your Name <knnymrls@outlook.com>
2024-06-19 00:04:38 -0500

    Added the new Searchbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { SearchBar } from "./misc/SearchBar";
 
 export default function NavBar() {
   return (
     <>
-    <main className="flex justify-end w-full max-h-fit">
+    <main className="flex justify-end w-full gap-4 max-h-fit">
         {/* Search Bar*/}
-        <div></div>
+        <SearchBar />
         {/* Dark Mode Button */}
-        <button className="rounded-full size-12 bg-primary-tw"></button>
+        <button className="flex items-center justify-center rounded-full size-12 bg-primary-tw"><img className="size-8" src={process.env.PUBLIC_URL + '/assets/add-icon.svg'} alt="" /></button>
         { /* Profile */}
-        <button className="rounded-full size-12 bg-primary-tw"></button>
+        <button
+  className="rounded-full size-12 bg-primary-tw"
+  style={{
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/base-profile.jpeg'})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  }}
+></button>
     </main>
     </>
   );
