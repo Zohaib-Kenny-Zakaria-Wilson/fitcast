6365f749a7c3622e5b252e5bffff3716883e0c58
Your Name <knnymrls@outlook.com>
2024-06-19 17:30:30 -0500

    Removed old components and simply routed the dailyoutfitpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Wthr from './components/Wthr';
-import TodayOutfitContainer from './components/TodayOutfitContainer';
 import Wardrobe from './components/Wardrobe';
-import NavBar from './components/navBar';
+import DailyOutfitPage from './pages/DailyOutfitPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Main Route */}
+        {/* DailyOutfit Route */}
         <Route path="/" element={
-          <main className={`w-screen flex gap-14 h-screen px-14 py-10 bg-background dark:bg-dark-background`}>
-            <Wthr />
-            <div className="flex flex-col w-full h-screen rounded-lg gap-9 bg-background dark:bg-dark-background">
-            <NavBar />
-            <TodayOutfitContainer />
-            </div>  
-          </main>
+           <DailyOutfitPage />
         } />
         {/* Wardrobe Route */}
         <Route path="/wardrobe" element={<Wardrobe />} />
