21f33366451e658712d63979b0828c89da6170fe
Kenny Morales <kenny-morales@outlook.com>
2024-06-19 11:06:08 -0500

    chore: Update DarkModeButton component to use custom hook for dark mode support

--- src/components/misc/DarkModeButton.jsx
+++ src/components/misc/DarkModeButton.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
+import useDarkMode from '../../hooks/useDarkMode';
 
-export function DarkModeButton({ onClick }) {
+export function DarkModeButton() {
+  const [isDarkMode, toggleDarkMode] = useDarkMode();
+
   return (
-    <button onClick={onClick} className="flex items-center justify-center w-12 h-12 bg-gray-800 rounded-full">
-      <svg
-        className="w-8 h-8 text-primary-tw dark:text-dark-primary-tw"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-      >
-        <path
-          fill="currentColor"
-          d="M11.01 3.05C6.51 3.54 3 7.36 3 12a9 9 0 0 0 9 9c4.63 0 8.45-3.5 8.95-8c.09-.79-.78-1.42-1.54-.95A5.403 5.403 0 0 1 11.1 7.5c0-1.06.31-2.06.84-2.89c.45-.67-.04-1.63-.93-1.56"
-        />
-      </svg>
+    <button onClick={toggleDarkMode} className="flex items-center justify-center w-12 h-12 bg-gray-800 rounded-full">
+      {isDarkMode ? (
+        <svg
+          className="w-8 h-8 text-primary-tw dark:text-dark-primary-tw"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5s5-2.24 5-5s-2.24-5-5-5M2 13h2c.55 0 1-.45 1-1s-.45-1-1-1H2c-.55 0-1 .45-1 1s.45 1 1 1m18 0h2c.55 0 1-.45 1-1s-.45-1-1-1h-2c-.55 0-1 .45-1 1s.45 1 1 1M11 2v2c0 .55.45 1 1 1s1-.45 1-1V2c0-.55-.45-1-1-1s-1 .45-1 1m0 18v2c0 .55.45 1 1 1s1-.45 1-1v-2c0-.55-.45-1-1-1s-1 .45-1 1M5.99 4.58a.996.996 0 0 0-1.41 0a.996.996 0 0 0 0 1.41l1.06 1.06c.39.39 1.03.39 1.41 0s.39-1.03 0-1.41zm12.37 12.37a.996.996 0 0 0-1.41 0a.996.996 0 0 0 0 1.41l1.06 1.06c.39.39 1.03.39 1.41 0a.996.996 0 0 0 0-1.41zm1.06-10.96a.996.996 0 0 0 0-1.41a.996.996 0 0 0-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0zM7.05 18.36a.996.996 0 0 0 0-1.41a.996.996 0 0 0-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0z"
+          />
+        </svg>
+      ) : (
+        <svg
+          className="w-8 h-8 text-primary-tw dark:text-dark-primary-tw"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M11.01 3.05C6.51 3.54 3 7.36 3 12a9 9 0 0 0 9 9c4.63 0 8.45-3.5 8.95-8c.09-.79-.78-1.42-1.54-.95A5.403 5.403 0 0 1 11.1 7.5c0-1.06.31-2.06.84-2.89c.45-.67-.04-1.63-.93-1.56"
+          />
+        </svg>
+      )}
     </button>
   );
 }
