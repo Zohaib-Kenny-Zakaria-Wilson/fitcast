5045f3390f9386cc9bc7e742b9106e9eb03c049f
Your Name <knnymrls@outlook.com>
2024-06-19 00:33:56 -0500

    Added NavBar to MainDisplay

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Wthr from './components/Wthr';
 import TodayOutfitContainer from './components/TodayOutfitContainer';
 import Wardrobe from './components/Wardrobe';
+import NavBar from './components/navBar';
 
 function App() {
   return (
@@ -12,7 +13,11 @@ function App() {
         <Route path="/" element={
           <main className={`w-screen flex gap-14 h-screen px-14 py-10 2xl:px-20 2xl:py-16 bg-background dark:bg-dark-background`}>
             <Wthr />
+            <div className="flex flex-col w-full h-screen rounded-lg gap-9 bg-background dark:bg-dark-background">
+            <NavBar />
             <TodayOutfitContainer />
+            </div>
+            
           </main>
         } />
         {/* Wardrobe Route */}
