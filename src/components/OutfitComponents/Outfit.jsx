878954c0fbc3dc5e0b3a721d604828601527f60b
Kenny Morales <kenny-morales@outlook.com>
2024-06-20 01:06:35 -0500

    Fixed Outfit Mistake I made

--- src/components/OutfitComponents/Outfit.jsx
+++ src/components/OutfitComponents/Outfit.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ClothingItem from "../ItemComponents/ClothingItem";
 
-export default function Outfit(clothingItems) {
+export default function Outfit({ clothingItems }) {
   return (
-    <>
-      <main className="grid min-w-full grid-cols-4 gap-12 p-12 rounded-lg dark:bg-dark-foreground bg-foreground">
-        {clothingItems &&
-          clothingItems.map((clothingItem) => {
-            return (
-              <ClothingItem clothingItem={clothingItem}/>
-            );
-          })}
-      </main>
-    </>
+    <main className="grid min-w-full grid-cols-4 gap-12 p-12 rounded-lg dark:bg-dark-foreground bg-foreground">
+      {clothingItems &&
+        clothingItems.map((clothingItem) => (
+          <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
+        ))}
+    </main>
   );
 }
