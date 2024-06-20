5bf4e7df9a4d692d1c2ae07816a757a4b77bc648
Your Name <knnymrls@outlook.com>
2024-06-19 21:53:29 -0500

    Used forecast for some things

--- src/components/PageComponents/Wthr.jsx
+++ src/components/PageComponents/Wthr.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import WeatherKeyInfo from "../WthrComponents/WthrStats";
 import { Logo } from "../misc/Logo";
 import useWeather from "../../hooks/useWeather";
-import useAstro from "../../hooks/useAstro";
+import useForecast from "../../hooks/useForecast";
 import { getWeatherIcon } from "../../utils/getWeatherIcon";
 
 function Wthr() {
   const { data } = useWeather();
-  const { astroData } = useAstro();
+  const { forecastData } = useForecast();
+
+
   return (
     <div className="grid h-full border-b-4 bg-foreground dark:bg-dark-foreground min-w-fit rounded-xl border-component-border dark:border-dark-component-border">
       <div className="flex flex-col items-center justify-between w-full h-full px-6 py-16 2xl:px-12">
@@ -25,7 +27,7 @@ function Wthr() {
             />
             <div className="flex flex-col xl:gap-1 2xl:gap-2">
               <p className="text-2xl font-bold text-center xl:text-3xl 2xl:text-5xl text-primary-tw dark:text-dark-primary-tw">
-                {data && data.current.feelslike_f}
+                {data && data.current.temp_f}
               </p>
               <p className="text-xs text-center xl:text-sm text-primary-tw dark:text-dark-primary-tw">
                 {data && data.current.condition.text}
@@ -33,23 +35,23 @@ function Wthr() {
             </div>
           </div>
           {/* Weather Details Wrapper */}
-          {data && astroData && (
+          {data && forecastData && (
             <div className="grid items-center grid-cols-2 gap-2 w-fit xl:gap-3 2xl:gap-3">
               {/* Weather Key Pair */}
               <WeatherKeyInfo
                 svg="sunrise"
-                text={astroData.astronomy.astro.sunrise}
+                text={forecastData.forecast.forecastday[0].astro.sunrise}
               />
               <WeatherKeyInfo
                 svg="sunset"
-                text={astroData.astronomy.astro.sunset}
+                text={forecastData.forecast.forecastday[0].astro.sunset}
               />
               {/* Weather Key Pair */}
-              <WeatherKeyInfo svg="thermometer" text={data.current.temp_f + "°F"} />
-              <WeatherKeyInfo svg="humidity" text={data.current.humidity } />
+              <WeatherKeyInfo svg="thermometer" text={forecastData.forecast.forecastday[0].day.avgtemp_f + "°F"} />
+              <WeatherKeyInfo svg="humidity" text={forecastData.forecast.forecastday[0].day.avghumidity + " %"} />
               {/* Weather Key Pair */}
               <WeatherKeyInfo svg="wind" text={data.current.wind_mph + " mph"} />
-              <WeatherKeyInfo svg="drizzle" text={data.current.precip_mm + " mm"} />
+              <WeatherKeyInfo svg="drizzle" text={forecastData.forecast.forecastday[0].day.daily_chance_of_rain + " %"} />
             </div>
           )}
         </div>
