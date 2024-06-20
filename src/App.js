fdbc3fcae918b276bfaebb90d3a71df460b7bad2
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-06-20 18:13:01 -0500

    adds main

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Wardrobe from './pages/Wardrobe';
-import DailyOutfitPage from './pages/DailyOutfitPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Wardrobe from "./pages/Wardrobe";
+import DailyOutfitPage from "./pages/DailyOutfitPage";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Main */}
+        <Route path="/" element={<Main />} />
         {/* DailyOutfit Route */}
-        <Route path="/" element={
-           <DailyOutfitPage />
-        } />
+        <Route path="/daily" element={<DailyOutfitPage />} />
         {/* Wardrobe Route */}
         <Route path="/wardrobe" element={<Wardrobe />} />
       </Routes>
