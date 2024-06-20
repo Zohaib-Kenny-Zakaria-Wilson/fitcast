854f440962fd27c7db93f77a4834c3929a9d2686
Zohaib2421 <zohaibshaikh2421@gmail.com>
2024-06-19 21:15:28 -0500

    Kind of have the filter working. It filters based on temperature.

--- src/components/OutfitComponents/Outfit.jsx
+++ src/components/OutfitComponents/Outfit.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ClothingItem from "../ItemComponents/ClothingItem";
 import useAppContext from "../../context/useAppContext";
+import useWeather from "../../hooks/useWeather";
 
 export default function Outfit() {
   const { clothingItems } = useAppContext();
+  const { data } = useWeather();
+  const temperature = data?.current.temp_f;
+  const filteredClothingItems = clothingItems?.filter((item) => (temperature > 60 && item?.hotWeather === true) || (temperature <= 60 && item?.coldWeather === true));
+
   return (
     <>
       <main className="flex justify-evenly">
-        {clothingItems &&
-          clothingItems.map((clothingItem) => {
-            return (
-              <ClothingItem clothingItem={clothingItem}/>
-            );
-          })}
+        {filteredClothingItems?.map((item) => (<ClothingItem clothingItem={item} />))}
       </main>
     </>
   );
