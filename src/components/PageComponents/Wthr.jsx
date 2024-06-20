8ef5ccbbfb375cddbb271b11b82dfb85eec13702
Your Name <knnymrls@outlook.com>
2024-06-20 17:13:43 -0500

    Added Loading and a Loading return

--- src/components/PageComponents/Wthr.jsx
+++ src/components/PageComponents/Wthr.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import WeatherKeyInfo from "../WthrComponents/WthrStats";
 import { Logo } from "../misc/Logo";
 import useCurrent from "../../hooks/useCurrent";
@@ -8,8 +8,8 @@ import useAppContext from "../../context/useAppContext";
 import getWthrConditions from "../../utils/getWthrConditions.js";
 
 function Wthr() {
-  const { currentData } = useCurrent();
-  const { forecastData } = useForecast();
+  const { currentData, isLoading: isCurrentLoading } = useCurrent();
+  const { forecastData, isLoading: isForecastLoading } = useForecast();
   const {
     setWthrConditions,
     globalCurrentData,
@@ -17,6 +17,8 @@ function Wthr() {
     setGlobalCurrentData,
     setGlobalForecastData,
   } = useAppContext();
+
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!globalCurrentData && !globalForecastData) {
@@ -28,7 +30,10 @@ function Wthr() {
           forecastData.forecast.forecastday[0].day
         );
         setWthrConditions(wthrConditions);
+        setIsLoading(false);
       }
+    } else {
+      setIsLoading(false);
     }
   }, [
     forecastData,
@@ -46,13 +51,59 @@ function Wthr() {
    */
   function refreshData() {
     if (forecastData && currentData) {
+      setIsLoading(true);
       setGlobalCurrentData(currentData);
       setGlobalForecastData(forecastData.forecast.forecastday[0]);
       const wthrConditions = getWthrConditions(
         forecastData.forecast.forecastday[0].day
       );
       setWthrConditions(wthrConditions);
+      setIsLoading(false);
     }
+  }
+
+  if (isLoading || isCurrentLoading || isForecastLoading) {
+    return (
+      <div className="grid h-full border-b-4 bg-foreground dark:bg-dark-foreground min-w-fit rounded-xl border-component-border dark:border-dark-component-border">
+        <div className="flex flex-col items-center justify-between w-full h-full px-6 py-16 2xl:px-12">
+          <div className="flex flex-col w-full gap-3 xl:gap-6 2xl:gap-9">
+            <div className="flex flex-col items-center w-full gap-3 xl:gap-4 2xl:gap-6">
+              <p className="text-xl font-medium text-center xl:text-2xl 2xl:text-4xl text-primary-tw dark:text-dark-primary-tw">
+                Loading...
+              </p>
+              <img
+                className="size-24 xl:size-36 2xl:size-48"
+                src={process.env.PUBLIC_URL + "/assets/not-available.svg"}
+                alt="Clear Day"
+              />
+              <div className="flex flex-col xl:gap-1 2xl:gap-2">
+                <p className="text-2xl font-bold text-center xl:text-3xl 2xl:text-5xl text-primary-tw dark:text-dark-primary-tw">
+                  Loading...
+                </p>
+                <p className="text-xs text-center xl:text-sm text-primary-tw dark:text-dark-primary-tw">
+                  Loading...
+                </p>
+              </div>
+            </div>
+            {/* Weather Details Wrapper */}
+            <div className="grid items-center grid-cols-2 gap-2 w-fit xl:gap-3 2xl:gap-3">
+              {/* Weather Key Pair */}
+              <WeatherKeyInfo svg="sunrise" text="Loading..." />
+              <WeatherKeyInfo svg="sunset" text="Loading..." />
+              {/* Weather Key Pair */}
+              <WeatherKeyInfo svg="thermometer" text="Loading..." />
+              <WeatherKeyInfo svg="humidity" text="Loading..." />
+              {/* Weather Key Pair */}
+              <WeatherKeyInfo svg="wind" text="Loading..." />
+              <WeatherKeyInfo svg="drizzle" text="Loading..." />
+            </div>
+          </div>
+
+          {/* Logo */}
+          <Logo onClick={refreshData} />
+        </div>
+      </div>
+    ); // Replace with your loading indicator
   }
 
   return (
@@ -115,7 +166,6 @@ function Wthr() {
             </div>
           )}
         </div>
-
         {/* Logo */}
         <Logo onClick={refreshData} />
       </div>
