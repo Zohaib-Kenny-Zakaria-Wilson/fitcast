ab3b26de9570537dfa16e4440f23735dc70b3641
Kenny Morales <kenny-morales@outlook.com>
2024-06-20 17:57:37 -0500

    Create k

--- src/components/PageComponents/Wthr.jsx
+++ src/components/PageComponents/Wthr.jsx
@@ -1,60 +1,174 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WeatherKeyInfo from "../WthrComponents/WthrStats";
 import { Logo } from "../misc/Logo";
-import useWeather from "../../hooks/useWeather";
-import useAstro from "../../hooks/useAstro";
+import useCurrent from "../../hooks/useCurrent";
+import useForecast from "../../hooks/useForecast";
 import { getWeatherIcon } from "../../utils/getWeatherIcon";
+import useAppContext from "../../context/useAppContext";
+import getWthrConditions from "../../utils/getWthrConditions.js";
 
 function Wthr() {
-  const { data } = useWeather();
-  const { astroData } = useAstro();
+  const { currentData, isLoading: isCurrentLoading } = useCurrent();
+  const { forecastData, isLoading: isForecastLoading } = useForecast();
+  const {
+    setWthrConditions,
+    globalCurrentData,
+    globalForecastData,
+    setGlobalCurrentData,
+    setGlobalForecastData,
+  } = useAppContext();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Testing
+  useEffect(() => {
+    if (!globalCurrentData && !globalForecastData) {
+      if (forecastData && currentData) {
+        // Set current and forecast data in the context
+        setGlobalCurrentData(currentData);
+        setGlobalForecastData(forecastData.forecast.forecastday[0]);
+        const wthrConditions = getWthrConditions(
+          forecastData.forecast.forecastday[0].day
+        );
+        setWthrConditions(wthrConditions);
+        setIsLoading(false);
+      }
+    } else {
+      setIsLoading(false);
+    }
+  }, [
+    forecastData,
+    currentData,
+    setGlobalCurrentData,
+    setGlobalForecastData,
+    globalCurrentData,
+    globalForecastData,
+    setWthrConditions,
+  ]);
+
+  /**
+   * Refreshes the data in the context.
+   * @returns {void}
+   */
+  function refreshData() {
+    if (forecastData && currentData) {
+      setIsLoading(true);
+      setGlobalCurrentData(currentData);
+      setGlobalForecastData(forecastData.forecast.forecastday[0]);
+      const wthrConditions = getWthrConditions(
+        forecastData.forecast.forecastday[0].day
+      );
+      setWthrConditions(wthrConditions);
+      setIsLoading(false);
+    }
+  }
+
+  if (isLoading || isCurrentLoading || isForecastLoading) {
+    return (
+      <div className="grid h-full border-b-4 bg-foreground dark:bg-dark-foreground min-w-fit rounded-xl border-component-border dark:border-dark-component-border">
+        <div className="flex flex-col items-center justify-between w-full h-full px-6 py-16 2xl:px-12">
+          <div className="flex flex-col w-full gap-3 xl:gap-6 2xl:gap-9">
+            <div className="flex flex-col items-center w-full gap-3 xl:gap-4 2xl:gap-6">
+              <p className="text-xl font-medium text-center xl:text-2xl 2xl:text-4xl text-primary-tw dark:text-dark-primary-tw">
+                Loading...
+              </p>
+              <img
+                className="size-24 xl:size-36 2xl:size-48"
+                src={process.env.PUBLIC_URL + "/assets/not-available.svg"}
+                alt="Clear Day"
+              />
+              <div className="flex flex-col xl:gap-1 2xl:gap-2">
+                <p className="text-2xl font-bold text-center xl:text-3xl 2xl:text-5xl text-primary-tw dark:text-dark-primary-tw">
+                  Loading...
+                </p>
+                <p className="text-xs text-center xl:text-sm text-primary-tw dark:text-dark-primary-tw">
+                  Loading...
+                </p>
+              </div>
+            </div>
+            {/* Weather Details Wrapper */}
+            <div className="grid items-center grid-cols-2 gap-2 w-fit xl:gap-3 2xl:gap-3">
+              {/* Weather Key Pair */}
+              <WeatherKeyInfo svg="sunrise" text="Loading..." />
+              <WeatherKeyInfo svg="sunset" text="Loading..." />
+              {/* Weather Key Pair */}
+              <WeatherKeyInfo svg="thermometer" text="Loading..." />
+              <WeatherKeyInfo svg="humidity" text="Loading..." />
+              {/* Weather Key Pair */}
+              <WeatherKeyInfo svg="wind" text="Loading..." />
+              <WeatherKeyInfo svg="drizzle" text="Loading..." />
+            </div>
+          </div>
+
+          {/* Logo */}
+          <Logo onClick={refreshData} />
+        </div>
+      </div>
+    ); // Replace with your loading indicator
+  }
+
   return (
     <div className="grid h-full border-b-4 bg-foreground dark:bg-dark-foreground min-w-fit rounded-xl border-component-border dark:border-dark-component-border">
       <div className="flex flex-col items-center justify-between w-full h-full px-6 py-16 2xl:px-12">
         <div className="flex flex-col w-full gap-3 xl:gap-6 2xl:gap-9">
           <div className="flex flex-col items-center w-full gap-3 xl:gap-4 2xl:gap-6">
-            {data && (
+            {globalCurrentData && (
               <p className="text-xl font-medium text-center xl:text-2xl 2xl:text-4xl text-primary-tw dark:text-dark-primary-tw">
-                {data.location.name}
+                {globalCurrentData.location.name}
               </p>
             )}
             <img
               className="size-24 xl:size-36 2xl:size-48"
-              src={getWeatherIcon(data && data.current.condition.code, 1)}
+              src={getWeatherIcon(
+                globalCurrentData && globalCurrentData.current.condition.code,
+                1
+              )}
               alt="Clear Day"
             />
             <div className="flex flex-col xl:gap-1 2xl:gap-2">
               <p className="text-2xl font-bold text-center xl:text-3xl 2xl:text-5xl text-primary-tw dark:text-dark-primary-tw">
-                {data && data.current.feelslike_f}
+                {globalCurrentData && globalCurrentData.current.temp_f}
               </p>
               <p className="text-xs text-center xl:text-sm text-primary-tw dark:text-dark-primary-tw">
-                {data && data.current.condition.text}
+                {globalCurrentData && globalCurrentData.current.condition.text}
               </p>
             </div>
           </div>
           {/* Weather Details Wrapper */}
-          {data && astroData && (
+          {globalCurrentData && globalForecastData && (
             <div className="grid items-center grid-cols-2 gap-2 w-fit xl:gap-3 2xl:gap-3">
               {/* Weather Key Pair */}
               <WeatherKeyInfo
                 svg="sunrise"
-                text={astroData.astronomy.astro.sunrise}
+                text={globalForecastData.astro.sunrise}
               />
               <WeatherKeyInfo
                 svg="sunset"
-                text={astroData.astronomy.astro.sunset}
+                text={globalForecastData.astro.sunset}
               />
               {/* Weather Key Pair */}
-              <WeatherKeyInfo svg="thermometer" text={data.current.temp_f + "°F"} />
-              <WeatherKeyInfo svg="humidity" text={data.current.humidity } />
+              <WeatherKeyInfo
+                svg="thermometer"
+                text={globalForecastData.day.avgtemp_f + "°F"}
+              />
+              <WeatherKeyInfo
+                svg="humidity"
+                text={globalForecastData.day.avghumidity + " %"}
+              />
               {/* Weather Key Pair */}
-              <WeatherKeyInfo svg="wind" text={data.current.wind_mph + " mph"} />
-              <WeatherKeyInfo svg="drizzle" text={data.current.precip_mm + " mm"} />
+              <WeatherKeyInfo
+                svg="wind"
+                text={globalForecastData.day.maxwind_mph + " mph"}
+              />
+              <WeatherKeyInfo
+                svg="drizzle"
+                text={globalForecastData.day.daily_chance_of_rain + " %"}
+              />
             </div>
           )}
         </div>
         {/* Logo */}
-        <Logo />
+        <Logo onClick={refreshData} />
       </div>
     </div>
   );
