e38ec330302407e5a658eee43833a6ad79e080e6
Your Name <knnymrls@outlook.com>
2024-06-19 21:53:06 -0500

    Created a new forecast hook similar to astro

---
+++ src/hooks/useForecast.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+export default function useAstro() {
+  const [forecastData, setforecastData] = useState(null);
+  const [error, setError] = useState(null);
+  const [coords, setCoords] = useState(null);
+
+  useEffect(() => {
+    async function init() {
+      let coordsVar;
+      navigator.geolocation.getCurrentPosition((position) => {
+        coordsVar =
+          position.coords.latitude.toFixed(4) +
+          "," +
+          position.coords.longitude.toFixed(4);
+        setCoords(coordsVar);
+      });
+      const baseUrl =
+        "http://api.weatherapi.com/v1/forecast.json?key=" +
+        process.env.REACT_APP_API_KEY +
+        "&q=" +
+        coords;
+      setforecastData(null);
+      setError(null);
+      try {
+        await fetch(baseUrl, {
+          mode: "cors",
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => setforecastData(data))
+          .catch((err) => console.error(err));
+      } catch (e) {
+        setError(e);
+      }
+    }
+    init();
+  }, [coords]);
+
+  return { forecastData, error };
+}
