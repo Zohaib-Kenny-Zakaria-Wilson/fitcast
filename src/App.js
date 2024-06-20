14e7cb7aee804850cda5fe64123434b4fc785cd1
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-06-19 22:01:46 -0500

    adds settings route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Wardrobe from './pages/Wardrobe';
-import DailyOutfitPage from './pages/DailyOutfitPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Wardrobe from "./pages/Wardrobe";
+import DailyOutfitPage from "./pages/DailyOutfitPage";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* DailyOutfit Route */}
-        <Route path="/" element={
-           <DailyOutfitPage />
-        } />
+        <Route path="/" element={<DailyOutfitPage />} />
         {/* Wardrobe Route */}
         <Route path="/wardrobe" element={<Wardrobe />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );
