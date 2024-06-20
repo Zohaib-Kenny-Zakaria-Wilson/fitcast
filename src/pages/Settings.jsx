24464cb4d79cc579e4825deab55ab6770be37035
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-06-19 22:27:35 -0500

    builds out a base settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Settings() {
-  return <div>Settings</div>;
+  return (
+    <main className=" flex flex-col justify-center my-48 mx-48 rounded-lg dark:bg-dark-foreground h-2/4 p-8 gap-9">
+      <div className="text-4xl ">Settings</div>
+      <div>
+        <button className="px-4 py-2 rounded dark:text-dark-primary-tw dark:bg-dark-component-border hover:bg-blue-700">
+          sign out
+        </button>
+        <Link
+          to="/"
+          className="px-4 py-2 rounded dark:text-dark-primary-tw dark:bg-dark-component-border hover:bg-blue-700"
+        >
+          save
+        </Link>
+      </div>
+    </main>
+  );
 }
 
 export default Settings;
