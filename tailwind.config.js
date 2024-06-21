ca82a9f685861ceee86bbf8dc6e020c020458e82
Your Name <knnymrls@outlook.com>
2024-06-20 21:52:35 -0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    extend: {
+      borderRadius: {
+        sm: "8px",
+        md: "16px",
+      },
+    },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'background': '#f0f0f0',
-      'foreground': '#ffffff',
-      'primary-tw': '#494747',
-      'secondary-tw': '#7A7A7A',
-      'component-border': '#DFDFDF',
-      'dark-background': '#272626', // Dark mode background
-      'dark-foreground': '#373737', // Dark mode foreground
-      'dark-primary-tw': '#C7C7C7', // Dark mode primary-tw text
-      'dark-component-border': '#1A1A1A', // Dark mode border
+      transparent: "transparent",
+      current: "currentColor",
+      background: "#f0f0f0",
+      foreground: "#ffffff",
+      "primary-tw": "#494747",
+      "secondary-tw": "#7A7A7A",
+      "component-border": "#DFDFDF",
+      "dark-background": "#272626", // Dark mode background
+      "dark-foreground": "#373737", // Dark mode foreground
+      "dark-primary-tw": "#C7C7C7", // Dark mode primary-tw text
+      "dark-component-border": "#1A1A1A", // Dark mode border
     },
     fontFamily: {
-      'sans': ['azo-sans-web', 'sans-serif'],
+      sans: ["azo-sans-web", "sans-serif"],
     },
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
-
-//NOTICE: We can change default theme values in tailwind.config.js file. And make it custom
+  plugins: [require("daisyui")],
+};
