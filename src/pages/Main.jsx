b6868beb318967fe729c789481ebcd405fdee715
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-06-20 19:37:18 -0500

    creates main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,17 +1,23 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { Logo } from "../components/misc/Logo";
+import react from "../components/images/demo2.png";
 function Main() {
   return (
-    <main className="flex bg-gray-950 flex-row h-screen ">
-      <div className="flex-col grow justify-center px-5 pt-8 bg-gray-50">
-        <Logo />
+    <main className="flex bg-gray-950 md:flex-row lg:flex-row flex-col-reverse h-screen ">
+      <div></div>
+      <div className="flex flex-col justify-center px-5 pt-8 bg-background">
+        <img
+          alt="pci"
+          width="90%"
+          src={require("../components/images/demo2.png")}
+        ></img>
       </div>
-      <div className="flex flex-col items-center justify-center px-5 py-8 bg-emerald-400 gap-10 ">
-        <h1 className="text-5xl font-bold text-gray-950">Get Started</h1>
+      <div className="flex grow flex-col items-center justify-center p-12 bg-emerald-400 gap-10 ">
+        <h1 className="text-5xl font-bold text-gray-950"> Get Started With </h1>
+        <Logo />
         <div className="flex flex-row justify-between gap-x-3 gap-y-2">
-          <Link class="btn btn-large">Log in</Link>
-          <Link class="btn btn-large">Sign up</Link>
+          <Link className="btn btn-large">Log in</Link>
+          <Link className="btn btn-large">Sign up</Link>
         </div>
       </div>
     </main>
