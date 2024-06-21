407aa9399233aa3ceaa1681cc5675783d813e964
Your Name <knnymrls@outlook.com>
2024-06-20 21:13:55 -0500

    Bottom Border

--- src/components/OutfitComponents/Outfit.jsx
+++ src/components/OutfitComponents/Outfit.jsx
@@ -3,7 +3,7 @@ import ClothingItem from "../ItemComponents/ClothingItem";
 
 export default function Outfit({ clothingItems }) {
   return (
-    <main className="grid min-w-full grid-cols-4 gap-12 p-12 rounded-lg dark:bg-dark-foreground bg-foreground">
+    <main className="grid min-w-full grid-cols-4 gap-12 p-12 border-b-4 rounded-lg dark:bg-dark-foreground bg-foreground border-component-border dark:border-dark-component-border">
       {clothingItems &&
         clothingItems.map((clothingItem) => (
           <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
