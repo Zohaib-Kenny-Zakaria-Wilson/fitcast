fef41f51cc39671d3de13c43c451646cc9e328b7
Your Name <knnymrls@outlook.com>
2024-06-20 21:53:10 -0500

    Fixed Rounding

--- src/components/ItemComponents/ItemTag.jsx
+++ src/components/ItemComponents/ItemTag.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
-export default function ItemTag({ onClick, text, color, textColor, borderColor }) {
+export default function ItemTag({
+  onClick,
+  text,
+  color,
+  textColor,
+  borderColor,
+}) {
   return (
     <div
       onClick={onClick}
-      className="flex justify-center gap-2 px-2 py-2 border-2 rounded-lg cursor-pointer w-fit"
-      style={{ backgroundColor: color, color: textColor, borderColor: borderColor }}
+      className="flex justify-center gap-2 px-2 py-2 border-2 rounded-sm cursor-pointer w-fit"
+      style={{
+        backgroundColor: color,
+        color: textColor,
+        borderColor: borderColor,
+      }}
     >
       <p className="text-xs md:text-base">{text}</p>
     </div>
