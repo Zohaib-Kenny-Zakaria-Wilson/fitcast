951f6bf5c5ffe0690021e9fe01b772860d95f2e4
Your Name <knnymrls@outlook.com>
2024-06-20 21:53:13 -0500

    Fixed Rounding

--- src/components/misc/Button.jsx
+++ src/components/misc/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 function Button({ children }) {
   return (
-    <button className="px-4 py-2 rounded text-primary-tw dark:text-dark-primary-tw bg-component-border dark:bg-dark-component-border">
+    <button className="py-2 px-4 bg-component-border rounded-sm text-primary-tw border-b-2 hover:translate-y-0.5 hover:border-b-2 hover:border-transparent transition-all duration-100">
       {children}
     </button>
   );
