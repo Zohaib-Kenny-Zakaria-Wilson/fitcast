aec0f468ad11d1e3c553db72c434f756ed30f4ec
Your Name <knnymrls@outlook.com>
2024-06-20 21:53:40 -0500

    Fixed Rounding

--- src/components/WthrComponents/WthrStats.jsx
+++ src/components/WthrComponents/WthrStats.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export default function WthrStats({ svg, text }) {
   return (
-    <div className="flex justify-center w-full gap-2 px-1 py-2 rounded-lg xl:px-2 xl:py-3 2xl:px-4 2xl:py-4 bg-background dark:bg-dark-background text-primary-tw dark:text-dark-primary-tw">
+    <div className="flex justify-center w-full gap-2 px-1 py-2 rounded-sm xl:px-2 xl:py-3 2xl:px-4 2xl:py-4 bg-background dark:bg-dark-background text-primary-tw dark:text-dark-primary-tw">
       <img
         className="h-4 2xl:h-5"
         src={`${process.env.PUBLIC_URL}/assets/${svg}.svg`}
