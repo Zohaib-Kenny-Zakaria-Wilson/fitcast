96c2060a64d12f8f8fb305e0f6ae3c983977eb98
Your Name <knnymrls@outlook.com>
2024-06-22 18:18:17 -0500

    refactor: Improve responsiveness of ItemTag component

--- src/components/ItemComponents/ItemTag.jsx
+++ src/components/ItemComponents/ItemTag.jsx
@@ -6,18 +6,28 @@ export default function ItemTag({
   color,
   textColor,
   borderColor,
+  hoverColor,
+  hoverTextColor,
 }) {
   return (
     <div
       onClick={onClick}
-      className="flex justify-center gap-2 px-2 py-2 border-2 rounded-sm cursor-pointer w-fit"
+      className={`flex justify-center gap-2 px-2 py-2 transition-all transform duration-150 border-2 rounded-sm cursor-pointer w-fit hover:scale-105 hover:shadow-md`}
       style={{
         backgroundColor: color,
         color: textColor,
         borderColor: borderColor,
       }}
     >
-      <p className="text-xs md:text-base">{text}</p>
+      <p
+        className="text-xs transition-colors duration-150 md:text-base user-select-none"
+        style={{
+          backgroundColor: color,
+          color: textColor,
+        }}
+      >
+        {text}
+      </p>
     </div>
   );
 }
