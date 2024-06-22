f006dec84337e090a38da8bf43c9a748ef7b38cf
Your Name <knnymrls@outlook.com>
2024-06-22 17:54:08 -0500

    refactor: Improve responsiveness of TodayOutfitContainer component

--- src/components/PageComponents/TodayOutfitContainer.jsx
+++ src/components/PageComponents/TodayOutfitContainer.jsx
@@ -38,12 +38,12 @@ export default function TodayOutfitContainer() {
         <div className="flex gap-4">
           <button
             onClick={regenerateOutfits}
-            className="py-1 px-4 min-h-full bg-component-border rounded-sm text-primary-tw border-b-2 hover:translate-y-0.5 hover:border-b-2 hover:border-transparent transition-all duration-100"
+            className="py-1 px-4 min-h-full dark:bg-dark-foreground dark:text-dark-primary-tw dark:border-dark-component-border bg-component-border rounded-sm text-primary-tw border-b-2 hover:translate-y-0.5 hover:border-b-2 hover:dark:border-transparent hover:border-transparent transition-all duration-100"
           >
             Regenerate
           </button>
           <Link to="/wardrobe">
-            <button className="py-1 px-4 min-h-full rounded-sm text-primary-tw dark:text-dark-background bg-foreground border-component-border dark:bg-dark-primary-tw border-b-2 hover:translate-y-0.5 hover:border-b-2 hover:border-transparent transition-all duration-100">
+            <button className="py-1 px-4 min-h-full rounded-sm text-primary-tw dark:text-dark-background bg-foreground border-component-border dark:bg-dark-primary-tw border-b-2 dark:border-foreground hover:dark:border-transparent hover:translate-y-0.5 hover:border-b-2 hover:border-transparent transition-all duration-100">
               Wardrobe
             </button>
           </Link>
