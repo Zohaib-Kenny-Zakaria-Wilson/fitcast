d85d8f32572bcd60f21239192e53cdf493dd7f1c
Your Name <knnymrls@outlook.com>
2024-06-21 21:56:22 -0500

    refactor: Improve responsiveness of Button and Wardrobe components

--- src/components/misc/Button.jsx
+++ src/components/misc/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 function Button({ children }) {
   return (
-    <button className="py-2 px-4 bg-component-border rounded-sm text-primary-tw border-b-2 hover:translate-y-0.5 hover:border-b-2 hover:border-transparent transition-all duration-100">
+    <button className="py-1 px-4 bg-component-border rounded-sm text-primary-tw border-b-2 hover:translate-y-0.5 hover:border-b-2 hover:border-transparent transition-all duration-100">
       {children}
     </button>
   );
