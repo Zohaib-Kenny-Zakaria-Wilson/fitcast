ea1a237a8b8604672a4b2579601a9fc7e5bb77b4
Your Name <knnymrls@outlook.com>
2024-06-22 23:02:03 -0500

    refactor: Improve responsiveness of Button component

--- src/components/misc/Button.jsx
+++ src/components/misc/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 function Button({ children }) {
   return (
-    <button className="py-1 px-4 min-h-full dark:bg-dark-foreground dark:text-dark-primary-tw dark:border-dark-component-border bg-component-border rounded-sm text-primary-tw border-b-2 hover:translate-y-0.5 hover:border-b-2 hover:dark:border-transparent hover:border-transparent transition-all duration-100">
+    <button className="py-1 px-4 min-h-full text-sm md:text-base dark:bg-dark-foreground dark:text-dark-primary-tw dark:border-dark-component-border bg-component-border rounded-sm text-primary-tw border-b-2 hover:translate-y-0.5 hover:border-b-2 hover:dark:border-transparent hover:border-transparent transition-all duration-100">
       {children}
     </button>
   );
