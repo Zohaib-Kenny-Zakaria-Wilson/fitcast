6712dc95569d2e3e15221b91263e52394ab7e55a
Your Name <knnymrls@outlook.com>
2024-06-22 22:02:51 -0500

    refactor: Improve responsiveness of DailyOutfitPage component

--- src/pages/DailyOutfitPage.jsx
+++ src/pages/DailyOutfitPage.jsx
@@ -13,15 +13,15 @@ export default function DailyOutfitPage() {
   return (
     <div className="flex flex-col h-screen lg:p-10 lg:gap-6 xl:p-11 xl:gap-7 2xl:gap-8 2xl:p-12 lg:flex-row bg-background dark:bg-dark-background">
       {isWthrVisible && (
-        <div className="mx-4 mt-20 -mb-12 sm:mt-24 sm:-mb-8 sm:mx-12 2xl:m-0 lg:m-0 lg:static lg:w-fit lg:h-full lg:bg-transparent">
+        <div className="mx-6 mt-20 -mb-12 sm:mt-24 sm:-mb-8 sm:mx-12 2xl:m-0 lg:m-0 lg:static lg:w-fit lg:h-full lg:bg-transparent">
           <WthrContainer />
         </div>
       )}
       <div className="flex flex-1 w-full gap-3 overflow-auto scrollbar-hide lg:flex-col">
-        <div className="box-border fixed top-0 w-full px-4 py-4 sm:py-6 sm:px-12 z-1 lg:p-0 lg:static lg:pb-4">
+        <div className="box-border fixed top-0 w-full px-6 py-4 sm:py-6 sm:px-12 z-1 lg:p-0 lg:static lg:pb-4">
           <NavBarDaily toggleWthrDisplay={toggleWthrDisplay} />
         </div>
-        <div className="w-full mx-4 mt-20 lg:m-0 sm:mt-24 sm:mx-12">
+        <div className="w-full mx-6 mt-20 lg:m-0 sm:mt-24 sm:mx-12">
           <TodayOutfitContainer />
         </div>
       </div>
