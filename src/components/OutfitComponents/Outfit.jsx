ddb277a427a9b532cf4411e78ca496e4947ed129
Your Name <knnymrls@outlook.com>
2024-06-22 22:02:44 -0500

    refactor: Improve responsiveness of Outfit component

--- src/components/OutfitComponents/Outfit.jsx
+++ src/components/OutfitComponents/Outfit.jsx
@@ -3,7 +3,7 @@ import ClothingItem from "../ItemComponents/ClothingItem";
 
 export default function Outfit({ clothingItems }) {
   return (
-    <main className="grid min-w-full grid-cols-2 gap-12 px-16 py-12 border-b-2 rounded-md shadow-sm 2xl:gap-8 xl:px-20 2xl:p-12 2xl:grid-cols-4 border-component-border dark:border-dark-component-border dark:bg-dark-foreground bg-foreground">
+    <main className="grid min-w-full grid-cols-1 gap-12 px-12 py-10 border-b-2 rounded-md shadow-sm lg:py-12 lg:px-16 sm:grid-cols-2 2xl:gap-8 xl:px-20 2xl:p-12 2xl:grid-cols-4 border-component-border dark:border-dark-component-border dark:bg-dark-foreground bg-foreground">
       {clothingItems &&
         clothingItems.map((clothingItem) => (
           <ClothingItem key={clothingItem.id} clothingItem={clothingItem} />
