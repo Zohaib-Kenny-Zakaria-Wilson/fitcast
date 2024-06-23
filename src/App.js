ed0cd339b38c6853a8a93a09c68e2f7fe6e83ae5
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-06-22 23:00:36 -0500

    adds session to app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,33 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Wardrobe from "./pages/Wardrobe";
 import DailyOutfitPage from "./pages/DailyOutfitPage";
 import Main from "./pages/Main";
+import "./index.css";
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
 import Settings from "./pages/Settings";
+import useAppContext from "./context/useAppContext";
+const supabase = createClient(
+  "https://<project>.supabase.co",
+  "<your-anon-key>"
+);
 
 function App() {
+  const { setSession } = useAppContext();
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, [setSession]);
+
   return (
     <Router>
       <Routes>
