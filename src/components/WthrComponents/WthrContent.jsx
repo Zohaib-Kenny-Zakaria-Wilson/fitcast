06efabf3c2b8a954aff3f2c866146d07932ca89c
Your Name <knnymrls@outlook.com>
2024-06-22 23:13:19 -0500

    refactor: Remove unused WthrLogo component from WthrContent

--- src/components/WthrComponents/WthrContent.jsx
+++ src/components/WthrComponents/WthrContent.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { WthrLogo } from "./WthrLogo.jsx";
 import CurrentWthrContent from "./CurrentWthrContent.jsx";
 import ForecastWthrContent from "./ForecastWthrContent.jsx";
 
@@ -20,7 +19,6 @@ export default function WthrContent({
           loading={loading}
           globalForecastData={globalForecastData}
         />
-        <WthrLogo onClick={refreshData} />
       </div>
     </div>
   );
