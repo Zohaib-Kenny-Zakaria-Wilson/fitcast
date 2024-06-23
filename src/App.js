1a45c5bef959b8e50bbd5abecf5208e9df3136e1
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-06-22 23:54:23 -0500

    refactor: Remove unused Supabase authentication code in App.js

--- src/App.js
+++ src/App.js
@@ -9,30 +9,9 @@ import Wardrobe from "./pages/Wardrobe";
 import DailyOutfitPage from "./pages/DailyOutfitPage";
 import Main from "./pages/Main";
 import "./index.css";
-import { useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
 import Settings from "./pages/Settings";
-import useAppContext from "./context/useAppContext";
-
-const SUPABASE_URL = "https://batugplthlrnlthcjmqg.supabase.co";
-const supabase = createClient(SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY);
 
 function App() {
-  const { setSession } = useAppContext();
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => subscription.unsubscribe();
-  }, [setSession]);
-
   return (
     <Router>
       <Routes>
