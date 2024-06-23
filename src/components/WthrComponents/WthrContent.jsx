7ed20f0c318820400d01e58292bab788cc745e3c
Your Name <knnymrls@outlook.com>
2024-06-22 21:43:30 -0500

    refactor: Improve responsiveness of WthrContent component

--- src/components/WthrComponents/WthrContent.jsx
+++ src/components/WthrComponents/WthrContent.jsx
@@ -11,7 +11,7 @@ export default function WthrContent({
 }) {
   return (
     <div className="min-h-full border-b-4 rounded-md max-h-fit bg-foreground dark:bg-dark-foreground min-w-fit border-component-border dark:border-dark-component-border">
-      <div className="flex flex-col items-center justify-start w-full h-full gap-8 overflow-auto lg:p-8 xl:gap-12 2xl:gap-16 xl:p-10 2xl:p-12 scrollbar-hide">
+      <div className="flex flex-col items-center justify-start w-full h-full gap-4 p-5 overflow-auto md:gap-8 lg:flex-col md:flex-row lg:gap-8 md:p-8 lg:p-8 xl:gap-12 2xl:gap-16 xl:p-10 2xl:p-12 scrollbar-hide md:justify-center">
         <CurrentWthrContent
           loading={loading}
           globalCurrentData={globalCurrentData}
