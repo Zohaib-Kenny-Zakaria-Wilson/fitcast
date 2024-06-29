5fc1ca3731516b8b214294b9c536996279941e02
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-06-29 15:52:53 -0500

    feat: Add Google as a login provider in Auth component

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -37,6 +37,7 @@ function Main() {
               <Auth
                 supabaseClient={supabase}
                 appearance={{ theme: ThemeSupa }}
+                providers={["google"]}
               />
             </>
           )}
